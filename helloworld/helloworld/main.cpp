616b720b36d1b2dc4ea775ee5b2ba3f66a2c26b4
leon <luteresa@163.com>
2016-08-28 01:05:40 +0800

    add first  test codes

---
+++ helloworld/helloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by 余云波 on 16/8/28.
+//  Copyright © 2016年 余云波. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
